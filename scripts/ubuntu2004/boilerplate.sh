e0f67637fcb95468a11fca366d17411d21120bfe
Chris van Dal <cvandal@users.noreply.github.com>
2022-05-17 18:27:45 +1000

    remove commented out section for resolv.conf

--- scripts/ubuntu2004/boilerplate.sh
+++ scripts/ubuntu2004/boilerplate.sh
@@ -40,16 +40,10 @@ configure_http_proxy
 systemctl stop rsyslog irqbalance polkit
 partition_disks /dev/nvme1n1
 
+# use containerd instead of dockerd
 mkdir -p /etc/eks/containerd
 mv /tmp/containerd-config.toml /etc/eks/containerd
 mv /tmp/kubelet-containerd.service /etc/eks/containerd
 mv /tmp/pull-sandbox-image.sh /etc/eks/containerd
 mv /tmp/sandbox-image.service /etc/eks/containerd
 chmod +x /etc/eks/containerd/pull-sandbox-image.sh
-
-# Setting resolv.conf for the node
-#rm -rf /etc/resolv.conf
-#sed -i 's/#DNS=/DNS=10.1.0.2/g' /run/systemd/resolve/resolv.conf
-#ln -sf /run/systemd/resolve/resolv.conf /etc/resolv.conf
-#sudo systemctl restart systemd-resolved
-#reboot
